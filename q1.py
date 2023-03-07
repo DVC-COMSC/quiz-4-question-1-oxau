24346b2fd96a355c1240f1bc99caf0c4e34afebd
oxau <notoriousact@gmail.com>
2023-03-06 23:40:56 -0800

    x

--- q1.py
+++ q1.py
@@ -2,6 +2,26 @@
 # ******************************
 # Make your Code
 # ******************************
+iter = 0
+even = 0
+preval = 0
+while iter < 10:
+    val = int(input('Enter a number: '))
+    iter += 1
+    if (val%2) == 0:
+        val = preval
+    if val % 2 == 0 and preval % 2 == 0:
+        even +=1
+    else: 
+        even += 0
+print (even)
+
+
+    
+
+
+
+
 
 
 # Requirement
